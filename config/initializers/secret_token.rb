bfb63685a8e0909b9ba078c32532f7ee2d295f7b
George <george@dewar.co.nz>
2014-01-24 20:00:20 +1300

    Create skeletal Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Stockwatcher::Application.config.secret_key_base = '2ddeb0b35536a3669cd631adcb0165ee3e0361b7e8e8c16b43bee0022f79b3bc7829d9c6800343f015d0c410e1ed679e4900dfbce7c62a28bb3385baec5fa8e6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Stockwatcher::Application.config.session_store :cookie_store, key: '_stockwatcher_session'
