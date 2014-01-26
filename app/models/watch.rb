5c49a6a23a3016b58e5f2d6b6075368edaef63dd
George Dewar <george.dewar@datacom.co.nz>
2014-01-27 12:50:34 +1300

    Fix Forgot Password page, validate threshold

--- app/models/watch.rb
+++ app/models/watch.rb
@@ -3,5 +3,5 @@ class Watch < ActiveRecord::Base
   belongs_to :stock
 
   validates :stock, presence: true
-  validates :threshold, presence: true
+  validates :threshold, presence: true, :numericality => { :greater_than => 0}
 end
