9e2549f2be7b1b378b9d6392e6c848f27e575b9c
George Dewar <george.dewar@datacom.co.nz>
2014-01-27 13:01:39 +1300

    Add Welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Stockwatcher::Application.routes.draw do
+
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'watches#index'
+  root 'welcome#index'
 
   resources :watches
 
