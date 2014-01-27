c015265c3b5ef7d025183fdf70add331f784961c
George <george@dewar.co.nz>
2014-01-27 23:25:46 +1300

    Fix production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,7 @@ Stockwatcher::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
@@ -77,4 +77,15 @@ Stockwatcher::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'stockwatcher.co.nz',
+    user_name:            'support@stockwatcher.co.nz',
+    password:             'Ky0D8BQLiQ6EFev',
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
 end
