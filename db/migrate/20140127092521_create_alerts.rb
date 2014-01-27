2332d6b363ee01743f92336247228734dc2f1e0d
George <george@dewar.co.nz>
2014-01-27 22:35:58 +1300

    Don't send more than one per day

--- db/migrate/20140127092521_create_alerts.rb
+++ db/migrate/20140127092521_create_alerts.rb
@@ -3,6 +3,9 @@ class CreateAlerts < ActiveRecord::Migration
     create_table :alerts do |t|
       t.references :watch, index: true
 
+      t.belongs_to :previous_quote
+      t.belongs_to :current_quote
+
       t.timestamps
     end
   end
