b1a45fe06b62cf6c75f8f6f19b27e5ba5580be21
George <george@dewar.co.nz>
2014-01-27 21:49:18 +1300

    Email content

--- app/mailers/alert_mailer.rb
+++ app/mailers/alert_mailer.rb
@@ -1,8 +1,15 @@
 class AlertMailer < ActionMailer::Base
-  default from: "george@dewar.co.nz"
+  default from: "Stock Watcher <support@stockwatcher.co.nz>"
 
-  def stock_alert(user)
-  	@user = user
-  	mail(to: @user.email, subject: 'Alert!')
+  def stock_alert(watch, yesterday_close, current_quote, diff, percent_change)
+  	@watch = watch
+  	@yesterday_close = yesterday_close
+  	@current_quote = current_quote
+
+  	@direction_up = diff > 0
+  	@diff = diff.abs
+  	@percent_change = percent_change.abs
+  	
+  	mail(to: @watch.user.email, subject: "Stock Watcher alert for #{@watch.stock.label}")
   end
 end
