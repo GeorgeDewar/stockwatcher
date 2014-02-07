1b0f8b776d35c6459d9deaf5b8f149a6e5a07feb
George Dewar <george@dewar.co.nz>
2014-02-07 22:39:23 +1300

    Tidy up code (#2)

--- app/mailers/alert_mailer.rb
+++ app/mailers/alert_mailer.rb
@@ -1,14 +1,14 @@
 class AlertMailer < ActionMailer::Base
-  default from: "Stock Watcher <support@stockwatcher.co.nz>"
+  default from: 'Stock Watcher <support@stockwatcher.co.nz>'
 
-  def stock_alert(watch, yesterday_close, current_quote, diff, percent_change)
+  def stock_alert(watch, yesterday_close, current_quote, daily_diff)
   	@watch = watch
   	@yesterday_close = yesterday_close
   	@current_quote = current_quote
 
-  	@direction_up = diff > 0
-  	@diff = diff.abs
-  	@percent_change = percent_change.abs
+  	@direction_up = daily_diff.direction == '+'
+  	@diff = daily_diff.diff.abs
+  	@percent_change = daily_diff.percent_change.abs
   	
   	mail(to: @watch.user.email, subject: "Stock Watcher alert for #{@watch.stock.label}")
   end
