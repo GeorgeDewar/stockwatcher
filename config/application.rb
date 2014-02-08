5b1889a28629469ff0313da25603242ccd90b6f6
George Dewar <george@dewar.co.nz>
2014-02-08 22:57:12 +1300

    Avoid deprecation warning for I18n

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,9 @@ module Stockwatcher
     config.active_record.default_timezone = :local
     config.active_record.time_zone_aware_attributes = false
 
+    # Avoid deprecation warning - use new default behaviour
+    config.i18n.enforce_available_locales = true
+
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
