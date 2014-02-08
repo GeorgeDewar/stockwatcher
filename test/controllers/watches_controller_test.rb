2e4a39a22b5020baa33cbfda6a45f09d7810038c
George Dewar <george@dewar.co.nz>
2014-02-08 22:52:05 +1300

    Improve watches_controller tests

--- test/controllers/watches_controller_test.rb
+++ test/controllers/watches_controller_test.rb
@@ -3,7 +3,8 @@ require 'test_helper'
 class WatchesControllerTest < ActionController::TestCase
   setup do
     sign_in users(:one)
-    @watch = watches(:one)
+    @my_watch = watches(:one)
+    @not_my_watch = watches(:three)
   end
 
   test "should get index" do
@@ -25,26 +26,30 @@ class WatchesControllerTest < ActionController::TestCase
     assert_redirected_to watches_path
   end
 
-  test "should show watch" do
-    get :show, id: @watch
-    assert_response :success
-  end
-
   test "should get edit" do
-    get :edit, id: @watch
+    get :edit, id: @my_watch
     assert_response :success
   end
 
   test "should update watch" do
-    patch :update, id: @watch, watch: { stock_id: stocks(:one), threshold: 2 }
+    patch :update, id: @my_watch, watch: { stock_id: stocks(:one), threshold: 2 }
     assert_redirected_to watches_path
   end
 
   test "should destroy watch" do
     assert_difference('Watch.count', -1) do
-      delete :destroy, id: @watch
+      delete :destroy, id: @my_watch
     end
 
     assert_redirected_to watches_path
+  end
+
+  test "should not allow access to other people's watches" do
+    assert_raises ActiveRecord::RecordNotFound do
+      get :edit, id: @not_my_watch
+    end
+    assert_raises ActiveRecord::RecordNotFound do
+      patch :update, id: @not_my_watch, watch: {stock_id: stocks(:one), threshold: 2 }
+    end
   end
 end
