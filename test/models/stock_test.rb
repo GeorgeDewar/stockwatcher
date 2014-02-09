ccf41e341945a1109bcd6d706238fb10c288528e
George Dewar <george@dewar.co.nz>
2014-02-09 20:32:20 +1300

    Test stocks

--- test/models/stock_test.rb
+++ test/models/stock_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class StockTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "should get latest quote" do
+    stock = stocks(:one)
+    assert stock.latest_quote.created_at.to_date == Date.today
+  end
+
+  test "should give error when there are no quotes" do
+    stock = stocks(:three)
+    assert_raises RuntimeError do
+      stock.latest_quote
+    end
+  end
+
 end
