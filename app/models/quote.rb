51a53fda5ed629da1a1d19cb9e3307dec0b93dc3
George Dewar <george@dewar.co.nz>
2014-03-08 12:41:56 +1300

    Compensate for Yahoo's incorrect date

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -21,6 +21,7 @@ class Quote < ActiveRecord::Base
   		  price = quote[1]
         last_trade_time = DateTime.strptime("#{quote[3]} #{quote[2]}", '%m/%d/%Y %H:%M%P')
         last_trade_time = ActiveSupport::TimeZone['America/New_York'].local_to_utc(last_trade_time).in_time_zone('Auckland')
+        last_trade_time = last_trade_time - 1.days # the time Yahoo gives is actually impossible; it is one day ahead
         prev_close = quote[4]
   		  Quote.new(:stock => Stock.find_by_code(code), :price => price,
                   :prev_close => prev_close, :last_trade_time => last_trade_time).save
@@ -46,7 +47,8 @@ class Quote < ActiveRecord::Base
       puts <<-eos
           \nChecking watch on '#{watch.stock.code}' for user '#{watch.user.email}'
           Yesterday's close: $#{sprintf('%0.2f', quote.prev_close)}
-          Current price: $#{sprintf('%0.2f', quote.price)} at #{quote.created_at.to_s}
+          Latest quote: $#{sprintf('%0.2f', quote.price)} at #{quote.created_at.to_s} [ID: #{quote.id}]
+          Last Trade Time: #{quote.last_trade_time.to_s}
           Difference: $#{sprintf('%0.2f', daily_diff.diff)}
           Change: #{sprintf('%0.2f', daily_diff.percent_change)}%
           Threshold: #{sprintf('%0.2f', watch.threshold)}%
