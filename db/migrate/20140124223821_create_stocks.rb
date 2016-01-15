ff57d43e8f3c6d0f0f59160f49164a47a07e3c4a
George Dewar <george@dewar.co.nz>
2016-01-16 12:59:48 +1300

    Fix migrations for Postgres

--- db/migrate/20140124223821_create_stocks.rb
+++ db/migrate/20140124223821_create_stocks.rb
@@ -1,8 +1,8 @@
 class CreateStocks < ActiveRecord::Migration
   def change
     create_table :stocks do |t|
-      t.string :code, limit: 3, :null => false
-      t.string :name, limit: 50, :null => false
+      t.string :code, :null => false
+      t.string :name, :null => false
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,10 @@
 
 ActiveRecord::Schema.define(version: 20140207220937) do
 
-  create_table "alerts", force: true do |t|
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "alerts", force: :cascade do |t|
     t.integer  "watch_id"
     t.integer  "previous_quote_id"
     t.integer  "current_quote_id"
@@ -21,9 +24,9 @@ ActiveRecord::Schema.define(version: 20140207220937) do
     t.datetime "updated_at"
   end
 
-  add_index "alerts", ["watch_id"], name: "index_alerts_on_watch_id"
+  add_index "alerts", ["watch_id"], name: "index_alerts_on_watch_id", using: :btree
 
-  create_table "quotes", force: true do |t|
+  create_table "quotes", force: :cascade do |t|
     t.integer  "stock_id"
     t.decimal  "price"
     t.datetime "created_at"
@@ -32,18 +35,18 @@ ActiveRecord::Schema.define(version: 20140207220937) do
     t.decimal  "prev_close"
   end
 
-  add_index "quotes", ["stock_id"], name: "index_quotes_on_stock_id"
+  add_index "quotes", ["stock_id"], name: "index_quotes_on_stock_id", using: :btree
 
-  create_table "stocks", force: true do |t|
+  create_table "stocks", force: :cascade do |t|
     t.string   "code",       limit: 3,  null: false
     t.string   "name",       limit: 50, null: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  add_index "stocks", ["code"], name: "index_stocks_on_code", unique: true
+  add_index "stocks", ["code"], name: "index_stocks_on_code", unique: true, using: :btree
 
-  create_table "users", force: true do |t|
+  create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "first_name"
     t.string   "last_name"
@@ -60,10 +63,10 @@ ActiveRecord::Schema.define(version: 20140207220937) do
     t.string   "last_sign_in_ip"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
-  create_table "watches", force: true do |t|
+  create_table "watches", force: :cascade do |t|
     t.decimal  "threshold",  null: false
     t.integer  "user_id",    null: false
     t.datetime "created_at"
@@ -71,7 +74,7 @@ ActiveRecord::Schema.define(version: 20140207220937) do
     t.integer  "stock_id",   null: false
   end
 
-  add_index "watches", ["stock_id"], name: "index_watches_on_stock_id"
-  add_index "watches", ["user_id"], name: "index_watches_on_user_id"
+  add_index "watches", ["stock_id"], name: "index_watches_on_stock_id", using: :btree
+  add_index "watches", ["user_id"], name: "index_watches_on_user_id", using: :btree
 
 end
